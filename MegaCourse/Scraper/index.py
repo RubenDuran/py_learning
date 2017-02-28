37ec94e403c28e5df4728c220d2d222b9bf9f6a4
Ruben Duran <rd@rubenduran.net>
2017-02-28 11:24:47 -0700

    Script update

--- MegaCourse/Scraper/index.py
+++ MegaCourse/Scraper/index.py
@@ -1,18 +1,33 @@
 import urllib2
 import re
 from bs4 import BeautifulSoup
+import random
 
-baseball = "http://www.baseballmonkey.com/article-common-baseball-terms-defined"
-page = urllib2.urlopen(baseball)
-soup = BeautifulSoup(page, "lxml")
-baseball_words = []
+sport_url = [
+    "https://en.wikipedia.org/wiki/Glossary_of_baseball",
+    "https://en.wikipedia.org/wiki/Glossary_of_basketball_terms",
+    "https://en.wikipedia.org/wiki/Glossary_of_association_football_terms",
+    "https://en.wikipedia.org/wiki/Glossary_of_American_football",
+    "https://en.wikipedia.org/wiki/Glossary_of_golf",
+    "https://en.wikipedia.org/wiki/Glossary_of_rugby_league_terms",
+    "https://en.wikipedia.org/wiki/Glossary_of_ice_hockey_terms"
+    ]
+words = []
 
-all_strong=soup.find_all("strong")
-for strong in all_strong:
-    s=strong.get_text()
-    s=re.sub('[!@#$:]', '', s)
-    baseball_words.append(s)
-    print s
+for sport in sport_url:
+    page = urllib2.urlopen(sport)
+    soup = BeautifulSoup(page, "lxml")
+    all_dt=soup.find_all("dt")
+    for dt in all_dt:
+        word=dt.get_text()
+        word=re.sub('[!@#$: ]', '', word)
+        words.append(word)
 
-print baseball_words
-print len(baseball_words)
+new_ipsum = 0
+ipsum_string = " "
+while new_ipsum < 50:
+    new_word = random.choice(words)
+    ipsum_string = ipsum_string + " " + new_word
+    new_ipsum = new_ipsum+1
+
+print "Sport ipsum " + ipsum_string.lower()
