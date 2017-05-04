93e6ad97952bd3027d52225c4ec4826b860e58ff
Ruben Duran <rd@rubenduran.net>
2017-05-04 14:45:46 -0700

    connected form with model

--- MegaCourse/Django_lev_2/ProTwo/appTwo/views.py
+++ MegaCourse/Django_lev_2/ProTwo/appTwo/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from appTwo.models import User
+# from appTwo.models import User
+from appTwo.forms import newUserForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'appTwo/index.html')
 
+
+
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users' :user_list}
-    return render(request, 'appTwo/users.html', context = user_dict)
+    form = newUserForm()
+    if request.method == 'POST':
+        form = newUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+    return render(request, 'appTwo/users.html', {'form': form})
+
+
+
+
+
+#
+# def users(request):
+#     user_list = User.objects.order_by('first_name')
+#     user_dict = {'users' :user_list}
+#     return render(request, 'appTwo/users.html', context = user_dict)
